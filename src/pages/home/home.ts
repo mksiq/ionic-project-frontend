09083ac4677ff652f8937bbc725318e78df5ecf4
MkSiq <maickelsiqueira@gmail.com>
2020-04-23 17:00:52 -0400

    create login and category pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  public login() {
+    this.navCtrl.setRoot('CategoriesPage');
+  }
 }
