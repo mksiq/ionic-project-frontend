7cb2862a173c35108b40a0ea6156e0aba5a96026
MkSiq <maickelsiqueira@gmail.com>
2020-04-26 11:58:57 -0400

    Showing user data and profile img

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService} from '../../services/storage.service';
+import { ClientDTO } from '../../models/customer.dto';
+import { CustomerService } from '../../services/domain/customer.service';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -10,17 +13,29 @@ import { StorageService} from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: string;
+  customer: ClientDTO;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public customerService: CustomerService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
-      this.email = localUser.email;
+      this.customerService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.customer = response;
+          this.getImageIfExists();
+        }, error => {})
     }
+  }
+
+  getImageIfExists() {
+    this.customerService.getImageFromBucket(this.customer.id)
+      .subscribe(response => {
+        this.customer.imgUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.customer.id}.jpg`;
+      }, error => {})
   }
 
 }
